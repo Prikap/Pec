55f8ee9ec62dfdeb1e0516dbdcaafaf3cd575466
Priyansh Kapadia <97601790+Prikap@users.noreply.github.com>
2023-04-23 07:40:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const friendList = document.querySelectorAll('.friend-list li');
+const chatHeader = document.querySelector('.chat-header h2');
+const chatBody = document.querySelector('.chat-body');
+
+friendList.forEach
